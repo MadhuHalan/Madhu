1fc08d2aa73cf5914c4a16c9ccc7abb0e9c60f07
MadhuHalan <gelmadhu@gmail.com>
2016-10-20 15:34:40 +0530

    Create alphebet.c

---
+++ alphebet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("enter the alphebet");
+scanf("%c",&c);
+if(c>='a'&&c<='z')||(c>='A'&&c<='B')
+{
+printf("%c is a alphebet",c);
+{
+else
+{ 
+printf("%c is not a alphebet",c);
+}
+return 0;
+}
